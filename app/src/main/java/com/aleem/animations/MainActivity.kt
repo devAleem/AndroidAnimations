cd77b7875962c65ae4eb83eff8ad6908c4c797a5
Aleem Awan <aleemawan366@outlook.com>
2022-06-07 17:02:03 +0500

    first commit

---
+++ app/src/main/java/com/aleem/animations/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.aleem.animations
+
+import android.animation.ObjectAnimator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
